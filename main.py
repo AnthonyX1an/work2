2bcda6fc9a3fd3eac09c1ef446719a39243c7ff9
AnthonyX1an <1017673846@qq.com>
2025-02-19 20:22:40 +0800

    comfirm all archi

--- main.py
+++ main.py
@@ -1,5 +1,55 @@
+import argparse
+import copy
+import os
+import random
+import sys
+import warnings
+import time, subprocess
+
 import numpy as np
-import torch
+import torch 
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 import torch.nn as nn
 import torch.nn.functional as F
+
+from parse import parse_method, parser_add_default_args, parser_add_main_args
+
+def get_gpu_memory_map():
+    """Get the current gpu usage.
+    Returns
+    -------
+    usage: dict
+        Keys are device ids as integers.
+        Values are memory usage as integers in MB.
+    """
+    result = subprocess.check_output(
+        [
+            'nvidia-smi', '--query-gpu=memory.used',
+            '--format=csv,nounits,noheader'
+        ], encoding='utf-8')
+    # Convert lines into a dictionary
+    gpu_memory = np.array([int(x) for x in result.strip().split('\n')])
+    # gpu_memory_map = dict(zip(range(len(gpu_memory)), gpu_memory))
+    return gpu_memory
+
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    
+### Parse args ###
+parser = argparse.ArgumentParser(description='General Training Pipeline')
+parser_add_main_args(parser)
+args = parser.parse_args()
+parser_add_default_args(args)
+print(args)
+
+fix_seed(args.seed)
+
+if args.cpu:
+    device = torch.device("cpu")
+else:
+    device = torch.device("cuda:" + str(args.device)
+                          ) if torch.cuda.is_available() else torch.device("cpu")
